5df97d630b10e82556a371f4fc38cd28dc0e9f88
m-cakradewa <mdimasaac@proton.me>
2026-04-05 02:14:01 +0200

    ye

--- hotel_pipeline.py
+++ hotel_pipeline.py
@@ -12,9 +12,3 @@ with DAG(
         task_id = "run_scraper",
         bash_command = "python /opt/umrah_hotels_pipeline/main.py"
     )
-#     run_dbt = BashOperator(
-#         task_id = "run_dbt_models",
-#         bash_command = "cd opt/umrah_hotels_pipeline/dbt_project && dbt run"
-#     )
-
-# scrape >> run_dbt
